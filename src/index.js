29c3e230ea9cbd8cdd8bf83aff53b1941d8388d7
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2022-06-22 09:32:28 -0300

    Index

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import arcApiRequestOptions from "./arcApiRequestOptions";
+import contentSourceToPromise from "./contentSourceToPromise";
+import dateTimeToTime from "./dateTimeToTime";
+import * as defaultAuthor from "./defaultAuthor";
+import * as errors from "./errors";
+import getAuthorById from "./getAuthorById";
+import getAuthorInfo from "./getAuthorInfo";
+import getDistanceBetweenCoords from "./getDistanceBetweenCoords";
+import getImage from "./getImage";
+import getNodeName from "./getNodeName";
+import getSectionClass from "./getSectionClass";
+import getSectionInfo from "./getSectionInfo";
+import getSectionSlug from "./getSectionSlug";
+import getSubSite from "./getSubSite";
+import getUrlBySite from "./getUrlBySite";
+import hasPaywall from "./hasPaywall";
+import isAmp from "./isAmp";
+import isNEU from "./isNEU";
+import isNoAds from "./isNoAds";
+import minimizeAuthor from "./minimizeAuthor";
+import * as objectProperties from "./objectProperties";
+import page from "./page";
+import * as params from "./params";
+import pushWhen from "./pushWhen";
+import sectionConfig from "./sectionConfig";
+import sectionNavigation from "./sectionNavigation";
+
+export default {
+    arcApiRequestOptions,
+    contentSourceToPromise,
+    dateTimeToTime,
+    defaultAuthor,
+    errors,
+    getAuthorById,
+    getAuthorInfo,
+    getDistanceBetweenCoords,
+    getImage,
+    getNodeName,
+    getSectionClass,
+    getSectionInfo,
+    getSectionSlug,
+    getSubSite,
+    getUrlBySite,
+    hasPaywall,
+    isAmp,
+    isNEU,
+    isNoAds,
+    minimizeAuthor,
+    objectProperties,
+    page,
+    params,
+    pushWhen,
+    sectionConfig,
+    sectionNavigation
+};
